9b6cd8ed736c99075d811651295aedd0761bd4e8
Saurabh <saurabh.d2106@gmail.com>
2024-09-11 13:06:47 +0000

    Assignment 3

---
+++ tests/dragndrop.spec.ts
@@ -0,0 +1,17 @@
+import {expect, test} from "@playwright/test"
+
+test("Drag and Drop", async ({page})=> {
+
+    await page.goto("https://jqueryui.com/droppable/")
+
+    const source =  page.frameLocator(".demo-frame").locator("#draggable");
+
+    const target = page.frameLocator(".demo-frame").locator("#droppable");
+
+    await source.dragTo(target);
+
+    const targetText = await target.textContent();
+
+    expect(targetText?.trim()).toEqual("Dropped!")
+
+})

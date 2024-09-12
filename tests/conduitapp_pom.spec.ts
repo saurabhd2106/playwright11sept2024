6440c4e3e529857b765d40988a638f1f30be2a3f
Saurabh <saurabh.d2106@gmail.com>
2024-09-12 12:06:37 +0000

    Data Driven testing

--- tests/conduitapp_pom.spec.ts
+++ tests/conduitapp_pom.spec.ts
@@ -1,31 +1,36 @@
-import {expect, test} from "@playwright/test"
+import { expect, test } from "@playwright/test"
 import { Loginpage } from "../pages/loginpage"
 import { ArticlePage } from "../pages/articlepage"
 
 test.describe("Conduit App Tests", () => {
 
-    test.beforeEach(async ({page}) => {
+    test.beforeEach(async ({ page }) => {
 
         await page.goto("http://localhost")
 
+    });
+
+    [
+
+        { username: "testuser", useremail: "testuser@test.com", userpassword: "testpassword" },
+
+        { username: "testuser1", useremail: "testuser1@test.com", userpassword: "testpassword" }
+
+    ].forEach(({ username, useremail, userpassword }) => {
+
+        test(`Login to the application - ${username}`, async ({ page }) => {
+
+            const loginpage = new Loginpage(page);
+
+            await loginpage.loginToApplication(useremail, userpassword);
+
+            await loginpage.verifyLogin(username);
+
+        })
+
     })
 
-    test("Login to the application", async ({ page }) => {
 
-        const loginpage = new Loginpage(page);
-
-        const useremail = "testuser@test.com"
-        const password = "testpassword"
-
-        const username = "testuser"
-
-       
-
-        await loginpage.loginToApplication(useremail, password);
-
-        await loginpage.verifyLogin(username);
-
-    })
 
     test("Add Article", async ({ page }) => {
 
@@ -40,9 +45,9 @@ test.describe("Conduit App Tests", () => {
 
         await loginpage.loginToApplication(useremail, password);
 
-        await  loginpage.verifyLogin(username);
+        await loginpage.verifyLogin(username);
 
-        await  articlepage.addArticle("Article on playwright", "Playwright description", "Article details", "Automation");
+        await articlepage.addArticle("Article on playwright", "Playwright description", "Article details", "Automation");
 
         await articlepage.verifyAddArticle();
     })

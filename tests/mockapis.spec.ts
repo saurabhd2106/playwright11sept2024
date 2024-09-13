fa70d9fa495d752d78c391e242d360e8aa86711d
Saurabh <saurabh.d2106@gmail.com>
2024-09-13 11:34:02 +0000

    API testing using playwright

--- tests/mockapis.spec.ts
+++ tests/mockapis.spec.ts
@@ -54,5 +54,45 @@ test.describe("Mocking APIs", async ()=>{
 
     })
 
+    
+
+    test("Update response", async ({page})=>{
+
+        await page.route("/api/tags", async route => {
+
+            const response = await route.fetch()
+
+            const responseInJson = await response.json()
+
+            const modifiedResponse = {
+
+                ...responseInJson,
+                tags: ["playwright", "selenium", "jmeter"]
+
+            };
+
+            route.fulfill({
+                status: 200,
+                contentType: 'application/json',
+                body: JSON.stringify(modifiedResponse)
+
+            })
+
+        })
+
+        await page.goto("http://localhost")
+
+        await expect(page.getByText("Popular Tags").first()).toBeVisible();
+
+        const allTags = await page.locator(".tag-list > a").all();
+
+        await expect((allTags).length).toBe(3);
+
+        const tagTexts = await Promise.all(allTags.map(async tag => (await tag.textContent())?.trim()));
+        expect(tagTexts).toEqual(['playwright', 'selenium', 'jmeter']);
+
+
+    })
+
 
 })
